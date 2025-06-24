f8cb9d0a344e5d2e946389ceb24ef4bf6551718d
Delennysg <Delennysg@Delennysg.lan>
2025-06-23 21:33:19 -0600

    Finalizacion semana 04

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,20 @@
+
+package com.tienda.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/") //url al que responde
+    public String index() {
+        return "index"; //Nombre de la vista en templateas a mostar 
+    }
+    
+    @RequestMapping("/informacion") //url al que responde
+    public String info() {
+        return "info"; //Nombre de la vista en templateas a mostar 
+    }
+}
